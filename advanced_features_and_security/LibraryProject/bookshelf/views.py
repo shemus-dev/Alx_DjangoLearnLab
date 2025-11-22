a79ab7350c31c4c6c071e3c67100422764f8e6c8
shemus-dev <shemmutuku4@gmail.com>
2025-11-22 14:23:55 +0300

    push

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
@@ -7,10 +6,11 @@ from .models import Book, Library
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import get_user_model
+from .forms import ExampleForm
 from .forms import (BookForm, SafeSearchForm, BookSearchForm, CustomUserCreationForm,
                    LibraryForm, BookBorrowForm, ContactForm, UserProfileForm,
-                   AdvancedBookFilterForm, ExampleForm  
-)
+                   AdvancedBookFilterForm)
+from django.views.generic.detail import DetailView
 from django.utils.decorators import method_decorator
 from django.db.models import Q
 from django.core.exceptions import PermissionDenied
