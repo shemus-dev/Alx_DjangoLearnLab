817f3ffe92bbc68303db378d4a46e2b86ad8a625
shemus-dev <shemmutuku4@gmail.com>
2025-11-28 10:18:08 +0300

    push

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView 
+from rest_framework.generics.ListAPIView import ListAPIView 
 from .models import Book
 # Create your views here.
 from .serializers import BookSerializer
