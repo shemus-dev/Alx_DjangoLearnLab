2b525b54d573a661da80be6ba785283c7aa5507b
shemus-dev <shemmutuku4@gmail.com>
2025-12-05 12:09:54 +0300

    Temp rename

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/', include('api.urls')),
+    # If you're using DRF's browsable API auth
+    path('api-auth/', include('rest_framework.urls')),
 ]
