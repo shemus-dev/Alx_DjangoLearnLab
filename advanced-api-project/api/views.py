f49b1b179ff66337281ff66fbaf9f0aeea8000d5
shemus-dev <shemmutuku4@gmail.com>
2025-12-05 12:06:02 +0300

    Temp rename

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics , permissions
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics 
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from .models import Book
 from rest_framework import status, filters
