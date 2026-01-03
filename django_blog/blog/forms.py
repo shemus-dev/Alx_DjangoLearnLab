c92615b074211465e863e28d8973a7bf1952c85b
shemus-dev <shemmutuku4@gmail.com>
2026-01-03 20:46:18 +0300

    push

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 
 class RegisterForm(UserCreationForm):
@@ -16,12 +16,51 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+class TagWidget(forms.TextInput):
+    def __init__(self, attrs=None):
+        default_attrs = {
+            'placeholder': 'Enter tags separated by commas'
+        }
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(attrs=default_attrs)
+
+        
+    def save(self, commit=True):
+        post = super().save(commit=False)
+
+        if commit:
+            post.save()
+
+        tag_names = self.cleaned_data.get('tags')
+
+        if tag_names:
+            tag_list = [name.strip() for name in tag_names.split(',')]
+            tags = []
+
+            for name in tag_list:
+                tag, created = Tag.objects.get_or_create(name=name)
+                tags.append(tag)
+
+            post.tags.set(tags)
+
+        return post
+
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(required=False)
     class Meta:
         model = Post
         fields = ['title', 'content','tags']
+        widgets = {
+            'tags': TagWidget()
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']  
+
+
+
+
+
